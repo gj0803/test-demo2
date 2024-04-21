28833db5eb67aac02d435bcf569c0d6dfe4d1373
gj0803 <gaojian_0803@163.com>
2024-04-21 09:55:50 +0800

    这是我第一次提交

---
+++ demo.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+// 获取div元素
+var myDiv = document.getElementById('box');
+ 
+// 为div元素添加点击事件监听
+myDiv.addEventListener('click', function() {
+    alert('Div被点击了！');
+});
+
+}
